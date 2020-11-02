396b81489c38a51aca529b45947ac66152c5f18f
Darek <darekpl9@gmail.com>
2020-11-02 11:10:58 +0100

    Populated map with all instructions

--- NES/CPU6502.hpp
+++ NES/CPU6502.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+
 #include <cstdint>
 #include <string>
 #include <map>
+
 
 class CPU6502
 {
@@ -10,6 +12,12 @@ class CPU6502
 	typedef void (CPU6502::*op_ptr)();
 	typedef void (CPU6502::*adrmod_ptr)();
 
+	enum AdditionalCycles {
+
+		Default,
+		PageCrossed, // +1 if to a new page
+		NewPage // +1 if branch succedes, +2 if to a new page
+	};
 
 	struct Instruction {
 
@@ -17,17 +25,17 @@ class CPU6502
 		op_ptr operation;
 		adrmod_ptr adr_mod;
 		uint8_t cycles;
-		bool add_cycle;
+		AdditionalCycles additional_cycle;
 
 		Instruction(std::string op_name, op_ptr operation,
-				    adrmod_ptr adr_mod, uint8_t cycles, bool add_cycle) : 
+				    adrmod_ptr adr_mod, uint8_t cycles, AdditionalCycles additional_cycle) : 
 									op_name(op_name), operation(operation),
-									adr_mod(adr_mod), cycles(cycles), add_cycle(add_cycle) {}
+									adr_mod(adr_mod), cycles(cycles), additional_cycle(additional_cycle) {}
 
 		Instruction(std::string op_name, op_ptr operation,
 					adrmod_ptr adr_mod, uint8_t cycles) :
 									op_name(op_name), operation(operation),
-									adr_mod(adr_mod), cycles(cycles), add_cycle(false) {}
+									adr_mod(adr_mod), cycles(cycles), additional_cycle(Default) {}
 
 		Instruction(){}
 	};
@@ -45,26 +53,44 @@ class CPU6502
 		                    // Interrupt disable, Zero flag, Carry flag
 
 		std::map<uint8_t, Instruction> op_map{
-
-			{0x69, Instruction("ADC_IMD", &CPU6502::ADC, &CPU6502::mod_imd, 2)},
-			{0x65, Instruction("ADC_ZP", &CPU6502::ADC, &CPU6502::mod_zp, 3)},
-			{0x75, Instruction("ADC_ZPX", &CPU6502::ADC, &CPU6502::mod_zpx, 4)},
-			{0x6D, Instruction("ADC_ABS", &CPU6502::ADC, &CPU6502::mod_abs, 4)},
-			{0x7D, Instruction("ADC_ABSX", &CPU6502::ADC, &CPU6502::mod_absx, 4, true)}, // +1 if page crossed
-			{0x79, Instruction("ADC_ABSY", &CPU6502::ADC, &CPU6502::mod_absy, 4, true)}, // +1
-			{0x61, Instruction("ADC_IIDRX", &CPU6502::ADC, &CPU6502::mod_iidrx, 6)}, 
-			{0x71, Instruction("ADC_IDRIY", &CPU6502::ADC, &CPU6502::mod_idriy, 5, true)}, // +1
-
-			{0x29, Instruction("AND_IMD", &CPU6502::AND, &CPU6502::mod_imd, 2)},
-			{0x25, Instruction("AND_ZP", &CPU6502::AND, &CPU6502::mod_zp, 3)},
-			{0x35, Instruction("AND_ZPX", &CPU6502::AND, &CPU6502::mod_zpx, 4)},
-			{0x2D, Instruction("AND_ABS", &CPU6502::AND, &CPU6502::mod_abs, 4)},
-			{0x3D, Instruction("AND_ABSX", &CPU6502::AND, &CPU6502::mod_absx, 4, true)}, // +1
-			{0x39, Instruction("AND_ABSY", &CPU6502::AND, &CPU6502::mod_absy, 4, true)},
-			{0x21, Instruction("AND_IIDRX", &CPU6502::AND, &CPU6502::mod_iidrx, 6)},
-			{0x31, Instruction("AND_IDRIY", &CPU6502::AND, &CPU6502::mod_iidrx, 5, true)}
-
-		};
+				
+			{0x69, Instruction("ADC_IMD", &CPU6502::ADC, &CPU6502::mod_imd, 2)},						{0x18, Instruction("CLC_IMP", &CPU6502::CLC, &CPU6502::mod_imp, 2)},							{0x49, Instruction("EOR_IMD", &CPU6502::EOR, &CPU6502::mod_imd, 2)},							{0xA2, Instruction("LDX_IMD", &CPU6502::LDX, &CPU6502::mod_imd, 2)},							{0x2A, Instruction("ROL_ACC", &CPU6502::ROL, nullptr, 2)},										{0x86, Instruction("STX_ZP", &CPU6502::STX, &CPU6502::mod_zp, 3)},
+			{0x65, Instruction("ADC_ZP", &CPU6502::ADC, &CPU6502::mod_zp, 3)},							{0xD8, Instruction("CLD_IMP", &CPU6502::CLD, &CPU6502::mod_imp, 2)},							{0x45, Instruction("EOR_ZP", &CPU6502::EOR, &CPU6502::mod_zp, 3)},								{0xA6, Instruction("LDX_ZP", &CPU6502::LDX, &CPU6502::mod_zp, 3)},								{0x26, Instruction("ROL_ZP", &CPU6502::ROL, &CPU6502::mod_zp, 5)},								{0x96, Instruction("STX_ZPY", &CPU6502::STX, &CPU6502::mod_zpy, 4)},
+			{0x75, Instruction("ADC_ZPX", &CPU6502::ADC, &CPU6502::mod_zpx, 4)},						{0x58, Instruction("CLI_IMP", &CPU6502::CLI, &CPU6502::mod_imp, 2)},							{0x55, Instruction("EOR_ZPX", &CPU6502::EOR, &CPU6502::mod_zpx, 4)},							{0xB6, Instruction("LDX_ZPY", &CPU6502::LDX, &CPU6502::mod_zpy, 4)},							{0x36, Instruction("ROL_ZPX", &CPU6502::ROL, &CPU6502::mod_zpx, 6)},							{0x8E, Instruction("STX_ABS", &CPU6502::STX, &CPU6502::mod_abs, 4)},
+			{0x6D, Instruction("ADC_ABS", &CPU6502::ADC, &CPU6502::mod_abs, 4)},						{0xB8, Instruction("CLV_IMP", &CPU6502::CLV, &CPU6502::mod_imp, 2)},							{0x4D, Instruction("EOR_ABS", &CPU6502::EOR, &CPU6502::mod_abs, 4)},							{0xAE, Instruction("LDX_ABS", &CPU6502::LDX, &CPU6502::mod_abs, 4)},							{0x2E, Instruction("ROL_ABS", &CPU6502::ROL, &CPU6502::mod_abs, 6)},							
+			{0x7D, Instruction("ADC_ABSX", &CPU6502::ADC, &CPU6502::mod_absx, 4, PageCrossed)},			{0x18, Instruction("CLC_IMP", &CPU6502::CLC, &CPU6502::mod_imp, 2)},							{0x5D, Instruction("EOR_ABSX", &CPU6502::EOR, &CPU6502::mod_absx, 4, PageCrossed)},				{0xBE, Instruction("LDX_ABSY", &CPU6502::LDX, &CPU6502::mod_absy, 4, PageCrossed)},				{0x3E, Instruction("ROL_ABSX", &CPU6502::ROL, &CPU6502::mod_absx, 7)},							{0x84, Instruction("STY_ZP", &CPU6502::STY, &CPU6502::mod_zp, 3)},							
+			{0x79, Instruction("ADC_ABSY", &CPU6502::ADC, &CPU6502::mod_absy, 4, PageCrossed)},																											{0x59, Instruction("EOR_ABSY", &CPU6502::EOR, &CPU6502::mod_absy, 4, PageCrossed)},																																																				{0x94, Instruction("STY_ZPX", &CPU6502::STY, &CPU6502::mod_zpx, 4)},
+			{0x61, Instruction("ADC_IIDRX", &CPU6502::ADC, &CPU6502::mod_iidrx, 6)},																													{0x41, Instruction("EOR_IIDRX", &CPU6502::EOR, &CPU6502::mod_iidrx, 6)},						{0xA0, Instruction("LDY_IMD", &CPU6502::LDY, &CPU6502::mod_imd, 2)},							{0x6A, Instruction("ROR_ACC", &CPU6502::ROR, nullptr, 2)},										{0x8C, Instruction("STY_ABS", &CPU6502::STY, &CPU6502::mod_abs, 4)},
+			{0x71, Instruction("ADC_IDRIY", &CPU6502::ADC, &CPU6502::mod_idriy, 5, PageCrossed)},																										{0x51, Instruction("EOR_IDRIY", &CPU6502::EOR, &CPU6502::mod_idriy, 5, PageCrossed)},			{0xA4, Instruction("LDY_ZP", &CPU6502::LDY, &CPU6502::mod_zp, 3)},								{0x66, Instruction("ROR_ZP", &CPU6502::ROR, &CPU6502::mod_zp, 5)},
+																																																																										{0xB4, Instruction("LDY_ZPX", &CPU6502::LDY, &CPU6502::mod_zpx, 4)},							{0x76, Instruction("ROR_ZPX", &CPU6502::ROR, &CPU6502::mod_zpx, 6)},							{0xAA, Instruction("TAX_IMP", &CPU6502::TAX, &CPU6502::mod_imp, 2)},
+			{0x29, Instruction("AND_IMD", &CPU6502::AND, &CPU6502::mod_imd, 2)},						{0xC9, Instruction("CMP_IMD", &CPU6502::CMP, &CPU6502::mod_imd, 2)},							{0xE6, Instruction("INC_ZP", &CPU6502::INC, &CPU6502::mod_zp, 5)},								{0xAC, Instruction("LDY_ABS", &CPU6502::LDY, &CPU6502::mod_abs, 4)},							{0x6E, Instruction("ROR_ABS", &CPU6502::ROR, &CPU6502::mod_abs, 6)},							{0xA8, Instruction("TAY_IMP", &CPU6502::TAY, &CPU6502::mod_imp, 2)},
+			{0x25, Instruction("AND_ZP", &CPU6502::AND, &CPU6502::mod_zp, 3)},							{0xC5, Instruction("CMP_ZP", &CPU6502::CMP, &CPU6502::mod_zp, 3)},								{0xF6, Instruction("INC_ZPX", &CPU6502::INC, &CPU6502::mod_zpx, 6)},							{0xBC, Instruction("LDY_IMD", &CPU6502::LDY, &CPU6502::mod_absx, 4, PageCrossed)},				{0x7E, Instruction("ROR_ABSX", &CPU6502::ROR, &CPU6502::mod_absx, 7)},							{0xBA, Instruction("TSX_IMP", &CPU6502::TSX, &CPU6502::mod_imp, 2)},
+			{0x35, Instruction("AND_ZPX", &CPU6502::AND, &CPU6502::mod_zpx, 4)},						{0xD5, Instruction("CMP_ZPX", &CPU6502::CMP, &CPU6502::mod_zpx, 4)},							{0xEE, Instruction("INC_ABS", &CPU6502::INC, &CPU6502::mod_abs, 6)},																																																							{0x8A, Instruction("TXA_IMP", &CPU6502::TXA, &CPU6502::mod_imp, 2)},
+			{0x2D, Instruction("AND_ABS", &CPU6502::AND, &CPU6502::mod_abs, 4)},						{0xCD, Instruction("CMP_ABS", &CPU6502::CMP, &CPU6502::mod_abs, 4)},							{0xFE, Instruction("INC_ABSX", &CPU6502::INC, &CPU6502::mod_absx, 7)},							{0x4A, Instruction("LSR_ACC", &CPU6502::LSR, nullptr, 2)},										{0x40, Instruction("RTI_IMP", &CPU6502::RTI, &CPU6502::mod_imp, 6)},							{0x9A, Instruction("TXS_IMP", &CPU6502::TXS, &CPU6502::mod_imp, 2)},
+			{0x3D, Instruction("AND_ABSX", &CPU6502::AND, &CPU6502::mod_absx, 4, PageCrossed)},			{0xDD, Instruction("CMP_ABSX", &CPU6502::CMP, &CPU6502::mod_absx, 4, PageCrossed)},																												{0x46, Instruction("LSR_ZP", &CPU6502::LSR, &CPU6502::mod_zp, 5)},								{0x60, Instruction("RTS_IMP", &CPU6502::RTS, &CPU6502::mod_imp, 6)},							{0x98, Instruction("TYA_IMP", &CPU6502::TYA, &CPU6502::mod_imp, 2)},
+			{0x39, Instruction("AND_ABSY", &CPU6502::AND, &CPU6502::mod_absy, 4, PageCrossed)},			{0xD9, Instruction("CMP_ABSY", &CPU6502::CMP, &CPU6502::mod_absy, 4, PageCrossed)},				{0xE8, Instruction("INX_IMP", &CPU6502::INX, &CPU6502::mod_imp, 2)},							{0x56, Instruction("LSR_ZPX", &CPU6502::LSR, &CPU6502::mod_zpx, 6)},
+			{0x21, Instruction("AND_IIDRX", &CPU6502::AND, &CPU6502::mod_iidrx, 6)},					{0xC1, Instruction("CMP_IIDRX", &CPU6502::CMP, &CPU6502::mod_iidrx, 6)},						{0xC8, Instruction("INY_IMP", &CPU6502::INY, &CPU6502::mod_imp, 2)},							{0x4E, Instruction("LSR_ABS", &CPU6502::LSR, &CPU6502::mod_abs, 6)},							{0xE9, Instruction("SBC_IMD", &CPU6502::SBC, &CPU6502::mod_imd, 2)},
+			{0x31, Instruction("AND_IDRIY", &CPU6502::AND, &CPU6502::mod_iidrx, 5, PageCrossed)},		{0xD1, Instruction("CMP_IDRIY", &CPU6502::CMP, &CPU6502::mod_idriy, 5, PageCrossed)},																											{0x5E, Instruction("LSR_ABSX", &CPU6502::LSR, &CPU6502::mod_absx, 7)},							{0xE5, Instruction("SBC_ZP", &CPU6502::SBC, &CPU6502::mod_zp, 3)},
+																																																																																																		{0xF5, Instruction("SBC_ZPX", &CPU6502::SBC, &CPU6502::mod_zpx, 4)},
+			{0x0A, Instruction("ASL_ACC", &CPU6502::ASL, nullptr, 2)},									{0xE0, Instruction("CPX_IMD", &CPU6502::CPX, &CPU6502::mod_imd, 2)},							{0x4C, Instruction("JMP_ABS", &CPU6502::JMP, &CPU6502::mod_abs, 3)},							{0xEA, Instruction("NOP_IMP", &CPU6502::NOP, &CPU6502::mod_imp, 2)},							{0xED, Instruction("SBC_ABS", &CPU6502::SBC, &CPU6502::mod_abs, 4)},
+			{0x06, Instruction("ASL_ZP", &CPU6502::ASL, &CPU6502::mod_zp, 5)},							{0xE4, Instruction("CPX_ZP", &CPU6502::CPX, &CPU6502::mod_zp, 3)},								{0x6C, Instruction("JMP_IDR", &CPU6502::JMP, &CPU6502::mod_idr, 5)},																															{0xFD, Instruction("SBC_ABSX", &CPU6502::SBC, &CPU6502::mod_absx, 4, PageCrossed)},
+			{0x16, Instruction("ASL_ZPX", &CPU6502::ASL, &CPU6502::mod_zpx, 6)},						{0xEC, Instruction("CPX_ABS", &CPU6502::CPX, &CPU6502::mod_abs, 4)},							{0x20, Instruction("JSR_ABS", &CPU6502::JMP, &CPU6502::mod_abs, 6)},							{0x09, Instruction("ORA_IMD", &CPU6502::ORA, &CPU6502::mod_imd, 2)},							{0xF9, Instruction("SBC_ABSY", &CPU6502::SBC, &CPU6502::mod_absy, 4, PageCrossed)},
+			{0x0E, Instruction("ASL_ABS", &CPU6502::ASL, &CPU6502::mod_abs, 6)},																																																						{0x05, Instruction("ORA_ZP", &CPU6502::ORA, &CPU6502::mod_zp, 3)},								{0xE1, Instruction("SBC_IIDRX", &CPU6502::SBC, &CPU6502::mod_iidrx, 6)},
+			{0x1E, Instruction("ASL_ABSX", &CPU6502::ASL, &CPU6502::mod_absx, 7)},																																																						{0x15, Instruction("ORA_ZPX", &CPU6502::ORA, &CPU6502::mod_zpx, 4)},							{0xF1, Instruction("SBC_IDRIY", &CPU6502::SBC, &CPU6502::mod_idriy, 5, PageCrossed)},
+																																																																										{0x0D, Instruction("ORA_ABS", &CPU6502::ORA, &CPU6502::mod_abs, 4)},
+			{0x90, Instruction("BCC_REL", &CPU6502::BCC, &CPU6502::mod_rel, 2, NewPage)},				{0xC0, Instruction("CPY_IMD", &CPU6502::CPY, &CPU6502::mod_imd, 2)},							{0xA9, Instruction("LDA_IMD", &CPU6502::LDA, &CPU6502::mod_imd, 2)},							{0x1D, Instruction("ORA_ABSX", &CPU6502::ORA, &CPU6502::mod_absx, 4, PageCrossed)},				{0x38, Instruction("SEC_IMP", &CPU6502::SEC, &CPU6502::mod_imp, 2)},
+			{0xB0, Instruction("BCS_REL", &CPU6502::BCS, &CPU6502::mod_rel, 2, NewPage)},				{0xC4, Instruction("CPY_ZP", &CPU6502::CPY, &CPU6502::mod_zp, 3)},								{0xA5, Instruction("LDA_ZP", &CPU6502::LDA, &CPU6502::mod_zp, 3)},								{0x19, Instruction("ORA_ABSY", &CPU6502::ORA, &CPU6502::mod_absy, 4, PageCrossed)},				{0xF8, Instruction("SED_IMP", &CPU6502::SED, &CPU6502::mod_imp, 2)},
+			{0xF0, Instruction("BEQ_REL", &CPU6502::BEQ, &CPU6502::mod_rel, 2, NewPage)},				{0xCC, Instruction("CPY_ABS", &CPU6502::CPY, &CPU6502::mod_abs, 4)},							{0xB5, Instruction("LDA_ZPX", &CPU6502::LDA, &CPU6502::mod_zpx, 4)},							{0x01, Instruction("ORA_IIDRX", &CPU6502::ORA, &CPU6502::mod_iidrx, 6)},						{0x78, Instruction("SEI_IMP", &CPU6502::SEI, &CPU6502::mod_imp, 2)},
+			{0x30, Instruction("BMI_REL", &CPU6502::BMI, &CPU6502::mod_rel, 2, NewPage)},																												{0xAD, Instruction("LDA_ABS", &CPU6502::LDA, &CPU6502::mod_abs, 4)},							{0x11, Instruction("ORA_IDRIY", &CPU6502::ORA, &CPU6502::mod_idriy, 5, PageCrossed)},
+			{0xD0, Instruction("BNE_REL", &CPU6502::BNE, &CPU6502::mod_rel, 2, NewPage)},				{0xC6, Instruction("DEC_ZP", &CPU6502::DEC, &CPU6502::mod_zp, 5)},								{0xBD, Instruction("LDA_ABSX", &CPU6502::LDA, &CPU6502::mod_absx, 4, PageCrossed)},																												{0x85, Instruction("STA_ZP", &CPU6502::STA, &CPU6502::mod_zp, 3)},
+			{0x10, Instruction("BPL_REL", &CPU6502::BPL, &CPU6502::mod_rel, 2, NewPage)},				{0xD6, Instruction("DEC_ZPX", &CPU6502::DEC, &CPU6502::mod_zpx, 6)},							{0xB9, Instruction("LDA_ABSY", &CPU6502::LDA, &CPU6502::mod_absy, 4, PageCrossed)},				{0x48, Instruction("PHA_IMP", &CPU6502::PHA, &CPU6502::mod_imp, 3)},							{0x95, Instruction("STA_ZPX", &CPU6502::STA, &CPU6502::mod_zpx, 4)},
+			{0x50, Instruction("BVC_REL", &CPU6502::BVC, &CPU6502::mod_rel, 2, NewPage)},				{0xCE, Instruction("DEC_ABS", &CPU6502::DEC, &CPU6502::mod_abs, 6)},							{0xA1, Instruction("LDA_IIDRX", &CPU6502::LDA, &CPU6502::mod_iidrx, 6)},						{0x08, Instruction("PHP_IMP", &CPU6502::PHP, &CPU6502::mod_imp, 3)},							{0x8D, Instruction("STA_ABS", &CPU6502::STA, &CPU6502::mod_abs, 4)},
+			{0x70, Instruction("BVS_REL", &CPU6502::BVS, &CPU6502::mod_rel, 2, NewPage)},				{0xDE, Instruction("DEC_ABSX", &CPU6502::DEC, &CPU6502::mod_absx, 7)},							{0xB1, Instruction("LDA_IDRIY", &CPU6502::LDA, &CPU6502::mod_idriy, 5, PageCrossed)},			{0x68, Instruction("PLA_IMP", &CPU6502::PLA, &CPU6502::mod_imp, 4)},							{0x9D, Instruction("STA_ABSX", &CPU6502::STA, &CPU6502::mod_absx, 5)},
+																																																																										{0x28, Instruction("PLP_IMP", &CPU6502::PLP, &CPU6502::mod_imp, 4)},							{0x99, Instruction("STA_ABSY", &CPU6502::STA, &CPU6502::mod_absy, 5)},
+			{0x24, Instruction("BIT_ZP", &CPU6502::BIT, &CPU6502::mod_zp, 3)},							{0xCA, Instruction("DEX_IMP", &CPU6502::DEX, &CPU6502::mod_imp, 2)},																																																							{0x81, Instruction("STA_IIDRX", &CPU6502::STA, &CPU6502::mod_iidrx, 6)},
+			{0x2C, Instruction("BIT_ABS", &CPU6502::BIT, &CPU6502::mod_abs, 4)},						{0x88, Instruction("DEY_IMP", &CPU6502::DEY, &CPU6502::mod_imp, 2)},																																																							{0x91, Instruction("STA_IDRIY", &CPU6502::STA, &CPU6502::mod_idriy, 6)},
+			{0x00, Instruction("BRK_IMP", &CPU6502::BRK, &CPU6502::mod_imp, 7)},																																																																																												
+		};																																																																																					
 
 
 	public:
